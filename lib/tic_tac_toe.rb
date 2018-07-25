b62962edb80bd5b863d0fd990464786502090f85
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-07-25 22:07:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -83,8 +83,6 @@ class TicTacToe
     end
   end
   
-=======
-  
   def won?
     WIN_COMBINATIONS.any? do |combo|
       if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
